60932b3759733cc6e879d65cf1b487d39e929a85
latifsk <latifsk39@gmail.com>
2023-01-10 21:35:41 +0000

    navbar

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -17,37 +17,74 @@ function Donate() {
 
         <div className="donate__body">
           <div className="donate__left">
-            <div className="donate__leftTitle">How donations work?</div>
 
-            <div className="donate__leftDescription">
-              There are various ways you can donate money to help.
-              <br /> The first option is to send money to the bank account
-              number below, the bank is Access Bank and the branch of the bank
-              is Legon.
-              <br /> The second option is through mobile money transactions. The
-              transaction is made to an MTN number.
+            <div className="flex justify-center items-center flex-col">
+
+              <div className="donate__leftTitle">How donations work?</div>
+
+              <div className="donate__leftDescription">
+                There are various ways you can donate to help.
+                The first option is to donate to the bank account
+                number below, the bank is Access Bank and the branch of the bank
+                is Legon.
+                <br /> The second option is through mobile money.
+              </div>
+
             </div>
 
-            <div className="donate__bankAccount">Bank account</div>
-            <div className="donate__accountNumber">1043000002798</div>
 
-            <div className="donate__bankName">Bank name</div>
-            <div className="donate__NameOfBank">Access Bank</div>
+</div>
 
-            <div className="donate__bankName">Branch name</div>
-            <div className="donate__NameOfBank">Legon Branch</div>
+
+
+        <div className="grid grid-cols-2 max-[640px]:grid-cols-1 place-items-center p-5">
+
+          {/* bank section */}
+
+          <div className="flex">
+            <img className="donate__NameOfBank h-52 w-fit object-contain p-5" alt="bank illustration" src={require("../assets/bank.jpg")} />
+
+            <div>
+                        <div style={{color: "rgb(231, 162, 50)"}} className=" text-2xl font-bold ">Bank transfer</div>
+
+                        <div className="donate__bankAccount mt-10">Bank account</div>
+                        <div className="donate__accountNumber">1043000002798</div>
+
+                        <div className="donate__bankName mt-10">Bank name</div>
+                        <div className="donate__NameOfBank">Access Bank</div>
+
+                        <div className="donate__bankName mt-10">Branch name</div>
+                        <div className="donate__NameOfBank">Legon Branch</div>
+            </div>
+
           </div>
 
-          <div className="donate__right">
-            <div className="donate__bankName">
-              <div>Using Mobile Money</div>
-              <div className="donate__bankName">Momo Number</div>
-              <div className="donate__NameOfBank">+233 (0) 247135558</div>
+          {/* momo */}
+          <div className="h-full max-[640px]:py-10  flex">
 
-              <div className="donate__bankName">Momo Number Name</div>
+          <img className="donate__NameOfBank h-52 w-44 p-5 object-contain" alt="bank illustration" src={require("../assets/momo.png")} />
+
+
+            <div className="donate__bankName h-full">
+              <div style={{color: "rgb(231, 162, 50)"}} className=" text-2xl font-bold ">Mobile Money</div>
+              <div className="donate__bankName mt-10">Momo Number</div>
+              <div className="donate__NameOfBank ">024 713 5558</div>
+
+              <div className="donate__bankName mt-10">Momo Number Name</div>
               <div className="donate__NameOfBank">Dwumfour Abdullai</div>
             </div>
           </div>
+
+        </div>
+
+
+
+
+        </div>
+  
+
+        <div className="flex justify-center p-5">
+          Do you have other things you would like to donate? <a href="/contact" className="text-blue-600 pl-2 hover:cursor-pointer">Contact us</a>
         </div>
 
         <Copyright />
