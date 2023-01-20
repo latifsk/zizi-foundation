a2e5fbd5bf607e119c0f9c824fc01d11e48913c9
Latif S.K <S@Latifs-MacBook-Pro.local>
2023-01-20 07:36:48 +0000

    major change to nav5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 
 import LandingPage from "./pages/home";
 import Aboutzizi from "./pages/aboutZizi";
@@ -13,52 +13,78 @@ import ProjectDetails from "./components/projectdetails";
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LandingPage />,
-      // errorElement: <Aboutzizi />
-    },
 
-    {
-      path: "/about",
-      element: <Aboutzizi />,
-    },
+  return(
+    <BrowserRouter basename="/zizifoundation" >
+      <Routes>
+        <Route exact path="/zizifoundation" element={<LandingPage/>}></Route>
+        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/about" element={<Aboutzizi/>}></Route>
+        <Route path="/join_us" element={<Joinzizi/>}></Route>
+        <Route path="/our_projects" element={<OurProjects/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/donate" element={<Donate/>}></Route>
+        <Route path="/project_details" element={<ProjectDetails/>}></Route>
 
-    {
-      path: "/join_us",
-      element: <Joinzizi />,
-    },
 
-    {
-      path: "/our_projects",
-      element: <OurProjects />,
-    },
 
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
 
-    {
-      path: "/donate",
-      element: <Donate />,
-    },
 
-    {
-      path: "/project_details",
-      element: <ProjectDetails />,
-    },
-  ]);
-
-  ReactDOM.createRoot(document.getElementById("root")).render(
-
-    // <React.StrictMode>
-    <BrowserRouter basename="/zizifoundation">
-      <RouterProvider router={router}  />
+      </Routes>
     </BrowserRouter>
-    // </React.StrictMode>
-  );
+  )
+
+
+
+
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <LandingPage />,
+  //     // errorElement: <Aboutzizi />
+  //   },
+
+  //   {
+  //     path: "/about",
+  //     element: <Aboutzizi />,
+  //   },
+
+  //   {
+  //     path: "/join_us",
+  //     element: <Joinzizi />,
+  //   },
+
+  //   {
+  //     path: "/our_projects",
+  //     element: <OurProjects />,
+  //   },
+
+  //   {
+  //     path: "/contact",
+  //     element: <Contact />,
+  //   },
+
+  //   {
+  //     path: "/donate",
+  //     element: <Donate />,
+  //   },
+
+  //   {
+  //     path: "/project_details",
+  //     element: <ProjectDetails />,
+  //   },
+  // ]);
+
+  // ReactDOM.createRoot(document.getElementById("root")).render(
+
+  //   // <React.StrictMode>
+  //   <BrowserRouter basename="/zizifoundation">
+  //     <RouterProvider router={router}  />
+  //   </BrowserRouter>
+  //   // </React.StrictMode>
+  // );
+
 }
 
 export default App;
